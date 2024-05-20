d5174c0d47ca1cb97ba0100c67e77b681f1bc932
0necloud <60743000+0necloud@users.noreply.github.com>
2024-05-20 23:33:51 +0800

    Separated Three.JS logic and smoothened audio bars

--- src/components/AudioVisualiser.vue
+++ src/components/AudioVisualiser.vue
@@ -1,11 +1,11 @@
 <template>
-  <div ref="canvasContainer" class="visualiser-container">
+  <div class="visualiser-wrapper">
     <input
+      @change="handleFileUpload"
       type="file"
       accept="audio/*"
-      @change="handleFileUpload"
-      hidden
       ref="fileInput"
+      hidden
     />
 
     <h1 id="title">{{ title }}</h1>
@@ -13,30 +13,31 @@
     <audio id="audio" controls></audio>
     <div id="out"></div>
 
+    <ThreeVisualiser :audioProp="audio" />
     <SpeedDial
       :tooltipOptions="{ position: 'left' }"
       :model="items"
-      mask
-      direction="up"
       :transitionDelay="80"
       :showIcon="speedDialIcon"
+      :style="'position: fixed; bottom: 90px; right: 10px; z-index: 15; text-decoration: none;'"
       hideIcon="pi pi-times"
       buttonClass="p-button-info"
-      :style="'position: fixed; bottom: 90px; right: 10px; z-index: 15; text-decoration: none;'"
+      direction="up"
+      mask
     />
   </div>
 </template>
 
 <script>
-import * as THREE from "three";
-
+import ThreeVisualiser from "./ThreeVisualiser.vue";
 export default {
   name: "AudioVisualiser",
+  components: {
+    ThreeVisualiser,
+  },
   data() {
     return {
       title: "Audio Visualiser",
-      animationFrameId: 0,
-      bars: [],
       items: [
         {
           label: "Upload MP3",
@@ -49,18 +50,21 @@ export default {
           command: () => this.triggerLoop(),
         },
       ],
+      audio: null,
       isLooping: false,
+      auraInterval: null,
     };
   },
   computed: {
     speedDialIcon() {
-      return this.isLooping ? "pi pi-sync pi-spin" : "pi pi-star-fill"
-    }
+      return this.isLooping ? "pi pi-sync pi-spin" : "pi pi-star-fill";
+    },
   },
   methods: {
     triggerFileUpload() {
       this.$refs.fileInput.click();
     },
+
     handleFileUpload(event) {
       this.audio = document.getElementById("audio");
       const file = event.target.files[0];
@@ -76,141 +80,35 @@ export default {
       this.audio.src = URL.createObjectURL(file);
 
       this.audio.load();
-      this.context.resume();
+
       this.audio.play();
 
-      this.play();
+      // Start the aura animation
+      this.audio.addEventListener("play", this.startAuraAnimation);
+
+      // Stop the aura animation
+      this.audio.addEventListener("pause", this.stopAuraAnimation);
     },
 
     triggerLoop() {
-      document.getElementById("audio").loop = !document.getElementById("audio").loop;
+      document.getElementById("audio").loop =
+        !document.getElementById("audio").loop;
       this.isLooping = !this.isLooping;
     },
 
-    // Audio starts playing
-    play() {
-      // ERROR: Failed to execute 'createMediaElementSource' on 'AudioContext': HTMLMediaElement already connected previously to a different MediaElementSourceNode.
-      // But it still works :D
-      this.src = this.context.createMediaElementSource(this.audio);
-
-      this.src.connect(this.analyser);
-
-      this.analyser.connect(this.context.destination);
-
-      this.analyser.fftSize = 512;
-
-      const bufferLength = this.analyser.frequencyBinCount;
-      this.dataArray = new Uint8Array(bufferLength);
-
-      this.initThreeJS();
-      this.createBars(bufferLength);
-
-      this.animate();
+    startAuraAnimation() {
+      // Start the interval for aura animation
+      this.auraInterval = setInterval(this.animateAura, 10); // Change interval duration as needed
     },
 
-    // Set up Three.JS camera, character halo
-    initThreeJS() {
-      this.scene = new THREE.Scene();
-      this.group = new THREE.Group();
-
-      // Setup camera
-      this.camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-      );
-      this.camera.position.set(0, 0, 100);
-      this.camera.lookAt(this.scene.position);
-      this.scene.add(this.camera);
-
-      this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-
-      const width = this.$refs.canvasContainer.clientWidth;
-      const height = this.$refs.canvasContainer.clientHeight;
-
-      this.renderer.setSize(width, height);
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.$refs.canvasContainer.appendChild(this.renderer.domElement);
-
-      // Create character halo
-      const planeGeometry = new THREE.RingGeometry(9.5, 11, 1000);
-      const planeMaterial = new THREE.MeshBasicMaterial({
-        color: 0x3de5ff,
-        side: THREE.DoubleSide,
-      });
-
-      this.plane = new THREE.Mesh(planeGeometry, planeMaterial);
-      this.plane.rotation.x = Math.PI / 4;
-      this.plane.position.x -= 1.5; // Halo X offset
-
-      this.group.add(this.plane);
-
-      this.scene.add(this.group);
-
-      const light = new THREE.AmbientLight(0xffffff, 1);
-
-      this.scene.add(light);
-
-      window.addEventListener("resize", this.onWindowResize);
+    stopAuraAnimation() {
+      // Clear the interval for aura animation
+      clearInterval(this.auraInterval);
+      document.getElementById("anime").style.filter = "none";
     },
 
-    // Generate audio bars
-    createBars(bufferLength) {
-      const barWidth = 2;
-      const barSpacing = 0.75;
-      const startX = -((bufferLength * (barWidth + barSpacing)) / 2);
-      const barMaterial = new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        opacity: 0.3,
-        transparent: true,
-      });
-
-      const canvasContainerHeight = this.$refs.canvasContainer.offsetHeight;
-      const bottomOffset = canvasContainerHeight * 0.5 - 42;
-
-      for (let i = 0; i < bufferLength; i++) {
-        const geometry = new THREE.BoxGeometry(barWidth, 1, 1);
-        const bar = new THREE.Mesh(geometry, barMaterial);
-
-        bar.position.x = startX + i * (barWidth + barSpacing);
-        bar.position.y = -canvasContainerHeight / 2 + bottomOffset;
-
-        this.bars.push(bar);
-        this.scene.add(bar);
-      }
-    },
-
-    // Animation loop
-    animate() {
-      this.animationFrameId = requestAnimationFrame(this.animate);
-
-      this.analyser.getByteFrequencyData(this.dataArray);
-
-      for (let i = 0; i < this.dataArray.length; i++) {
-        const scale = (this.dataArray[i] / 256) * 150;
-        this.bars[i].scale.y = scale;
-      }
-
-      // Halo Bounce
-      let average = 0;
-      for (let i = 0; i < this.dataArray.length; i++) {
-        average += this.dataArray[i];
-      }
-      average = average / this.dataArray.length;
-
-      const bounceHeight = (average / 256) * 5 + 2;
-      this.plane.position.y = bounceHeight;
-
-      // Halo Color Change
-      const ratio = (average / 256) * 1.5;
-      const startColor = new THREE.Color(0x3de5ff);
-      const endColor = new THREE.Color(0xffffff);
-      const currentColor = startColor.lerp(endColor, ratio);
-
-      this.plane.material.color.set(currentColor);
-
-      // Character Drop Shadow
+    animateAura() {
+      // Character drop shadow
       const dropShadowX = (Math.random() - 0.5) * 10;
       const dropShadowY = (Math.random() - 0.5) * 10;
       const dropShadowBlur = 5 + Math.random();
@@ -220,42 +118,13 @@ export default {
       document.getElementById(
         "anime"
       ).style.filter = `drop-shadow(${dropShadowX}px ${dropShadowY}px ${dropShadowBlur}px ${dropShadowColor})`;
-
-      this.renderer.render(this.scene, this.camera);
     },
-
-    // Handle change in window size
-    onWindowResize() {
-      const width = this.$refs.canvasContainer.clientWidth;
-      const height = this.$refs.canvasContainer.clientHeight;
-
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-
-      this.renderer.setSize(width, height);
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-    },
-  },
-
-
-  beforeUnmount() {
-    window.removeEventListener("resize", this.onWindowResize);
-    if (this.audioContext) {
-      this.audioContext.close();
-    }
-    cancelAnimationFrame(this.animationFrameId);
-  },
-
-
-  created() {
-    this.context = new AudioContext();
-    this.analyser = this.context.createAnalyser();
   },
 };
 </script>
 
 <style>
-.visualiser-container {
+.visualiser-wrapper {
   position: relative;
   width: 100%;
   height: 100vh;
@@ -306,22 +175,28 @@ audio {
   }
 }
 
+/* STYLING AUDIO CONTROL PANEL */
+audio::-webkit-media-controls-panel {
+  border-radius: 20%;
+}
+
 /* OVERWRITE PRIMEVUE COMPONENT STYLING */
 
 .p-speeddial-button.p-button.p-button-icon-only {
-  width: 3.5rem;
-  height: 3.5rem;
+  width: 4rem;
+  height: 4rem;
+  margin-top: 15px;
   background: rgba(114, 0, 80, 0.2);
   border: 1px solid rgba(245, 245, 245, 0.4);
 }
 
 .p-speeddial-action {
-  width: 3rem;
-  height: 3rem;
+  width: 3.3rem;
+  height: 3.3rem;
   background: #000;
   color: #fff;
   outline: 1px solid #fff;
-  margin-top: 10px;
+  margin-top: 5px;
   text-decoration: none;
   transition: background-color 0.2s, color 0.2s, box-shadow 0.2s;
 }
---
+++ src/components/ThreeVisualiser.vue
@@ -0,0 +1,200 @@
+<template>
+  <div ref="canvasContainer" class="visualiser-container"></div>
+</template>
+
+<script>
+import * as THREE from "three";
+
+export default {
+  data() {
+    return {
+      animationFrameId: 0,
+      bars: [],
+    };
+  },
+  props: {
+    audioProp: Object,
+  },
+  watch: {
+    audioProp() {
+      this.play();
+    },
+  },
+  methods: {
+    // Set up Three.JS camera, character halo
+    initThreeJS() {
+      this.scene = new THREE.Scene();
+      this.group = new THREE.Group();
+
+      // Setup camera
+      this.camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      this.camera.position.set(0, 0, 100);
+      this.camera.lookAt(this.scene.position);
+      this.scene.add(this.camera);
+
+      this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+
+      const width = this.$refs.canvasContainer.clientWidth;
+      const height = this.$refs.canvasContainer.clientHeight;
+
+      this.renderer.setSize(width, height);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+      this.$refs.canvasContainer.appendChild(this.renderer.domElement);
+
+      // Create character halo
+      const planeGeometry = new THREE.RingGeometry(9.5, 11, 1000);
+      const planeMaterial = new THREE.MeshBasicMaterial({
+        color: 0x3de5ff,
+        side: THREE.DoubleSide,
+      });
+
+      this.plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      this.plane.rotation.x = Math.PI / 4;
+      this.plane.position.x -= 1.5; // Halo X offset
+
+      this.group.add(this.plane);
+
+      this.scene.add(this.group);
+
+      const light = new THREE.AmbientLight(0xffffff, 1);
+
+      this.scene.add(light);
+
+      window.addEventListener("resize", this.onWindowResize);
+    },
+
+    // Generate audio bars
+    createBars(bufferLength) {
+      const barWidth = 2;
+      const barSpacing = 0.75;
+      const startX = -((bufferLength * (barWidth + barSpacing)) / 2);
+      const barMaterial = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.3,
+        transparent: true,
+      });
+
+      const canvasContainerHeight = this.$refs.canvasContainer.offsetHeight;
+      const bottomOffset = canvasContainerHeight * 0.5 - 42;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const geometry = new THREE.BoxGeometry(barWidth, 1, 1);
+        const bar = new THREE.Mesh(geometry, barMaterial);
+
+        bar.position.x = startX + i * (barWidth + barSpacing);
+        bar.position.y = -canvasContainerHeight / 2 + bottomOffset;
+
+        this.bars.push(bar);
+        this.scene.add(bar);
+      }
+    },
+
+    // Audio starts playing
+    play() {
+      this.context.resume();
+
+      // ERROR: Failed to execute 'createMediaElementSource' on 'AudioContext': HTMLMediaElement already connected previously to a different MediaElementSourceNode.
+      // But it still works :D
+      this.src = this.context.createMediaElementSource(this.audioProp);
+
+      this.src.connect(this.analyser);
+
+      this.analyser.connect(this.context.destination);
+
+      this.analyser.fftSize = 512;
+
+      const bufferLength = this.analyser.frequencyBinCount;
+      this.dataArray = new Uint8Array(bufferLength);
+
+      this.initThreeJS();
+      this.createBars(bufferLength);
+
+      this.animate();
+    },
+
+    // Animation loop
+    animate() {
+      this.animationFrameId = requestAnimationFrame(this.animate);
+
+      this.analyser.getByteFrequencyData(this.dataArray);
+
+      // Scale audio bars
+      const barCount = this.bars.length;
+      const bandSize = Math.floor(this.dataArray.length / barCount);
+      const threshold = 0.1; // Smoothing threshold factor
+
+      for (let i = 0; i < barCount; i++) {
+        let sum = 0;
+        for (let j = 0; j < bandSize; j++) {
+          sum += this.dataArray[i * bandSize + j];
+        }
+        const averageValue = sum / bandSize;
+        const targetScale = (averageValue / 256) * 150;
+
+        // Apply threshold smoothing
+        if (Math.abs(targetScale - this.bars[i].scale.y) > threshold) {
+          this.bars[i].scale.y += (targetScale - this.bars[i].scale.y) * 0.1;
+        }
+      }
+
+      // Halo bounce
+      let average = 0;
+      for (let i = 0; i < this.dataArray.length; i++) {
+        average += this.dataArray[i];
+      }
+      average = average / this.dataArray.length;
+
+      const bounceHeight = (average / 256) * 5 + 2;
+      this.plane.position.y = bounceHeight;
+
+      // Halo color change
+      const ratio = (average / 256) * 1.5;
+      const startColor = new THREE.Color(0x3de5ff);
+      const endColor = new THREE.Color(0xffffff);
+      const currentColor = startColor.lerp(endColor, ratio);
+
+      this.plane.material.color.set(currentColor);
+
+      this.renderer.render(this.scene, this.camera);
+    },
+
+    // Handle change in window size
+    onWindowResize() {
+      const width = this.$refs.canvasContainer.clientWidth;
+      const height = this.$refs.canvasContainer.clientHeight;
+
+      this.camera.aspect = width / height;
+      this.camera.updateProjectionMatrix();
+
+      this.renderer.setSize(width, height);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+    },
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.onWindowResize);
+    if (this.context) {
+      this.context.close();
+    }
+    cancelAnimationFrame(this.animationFrameId);
+  },
+
+  created() {
+    this.context = new AudioContext();
+    this.analyser = this.context.createAnalyser();
+  },
+};
+</script>
+
+<style scoped>
+.visualiser-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
